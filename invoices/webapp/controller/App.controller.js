6b014ec5251a38917ae788cc6d12fcf4d54b9fa1
Roger-Alfa-Git <rogervalderrabano@alfasolucionesti.com>
2024-12-04 17:58:40 +0000

    Proyecto_Invoices

---
+++ invoices/webapp/controller/App.controller.js
@@ -0,0 +1,21 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+    /**
+     * 
+     * @param {typeof sap.ui.core.mvc.Controller} Controller 
+     */
+    function (Controller) {
+        "use strict";
+
+        return Controller.extend("alfagroup.invoices.controller.App", {
+
+            onInit: function () {
+
+            },
+
+            onOpenDialogHeader: function(){
+                this.getOwnerComponent().openHelloDialog();
+            }
+        });
+    });
